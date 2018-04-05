98a1d2ee4bcc03d85a90c2cbece49639c2991d50
YuliaGrigorieva <miss000hara@gmail.com>
2018-04-05 19:51:48 +0300

    Use new API Client

--- src/manager/LoginManager.js
+++ src/manager/LoginManager.js
@@ -6,228 +6,212 @@
 
 import React from 'react';
 import {
-  DeviceEventEmitter,
-  Platform,
-  AppState
+	DeviceEventEmitter,
+	Platform,
+	AppState
 } from 'react-native';
 
-import { VoximplantLegacy } from 'react-native-voximplant';
+import { VoximplantLegacy, Voximplant, ClientEvents, Client, ClientState } from 'react-native-voximplant';
 import DefaultPreference from 'react-native-default-preference';
 import PushManager from './PushManager';
 import md5 from "react-native-md5";
 
 DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.ConnectionFailed,
-  (connectionFailed) => {
-    console.log('Connection failed: reason: ' + connectionFailed.reason);
-    loginManagerGlobal.connected = false;
-    loginManagerGlobal.emit('onConnectionFailed', connectionFailed.reason);
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.ConnectionSuccessful,
-  () => {
-    console.log('Connection successful');
-    loginManagerGlobal.connected = true;
-    if (loginManagerGlobal.processingPushNotification) {
-      DefaultPreference.get('usernameValue').then(
-        function (username) {
-          DefaultPreference.get('accessToken').then(
-            function (accessToken) {
-              VoximplantLegacy.loginUsingAccessToken(username + ".voximplant.com", accessToken);
-            });
-        });
-    } else if (loginManagerGlobal.username !== '' && loginManagerGlobal.password !== '') {
-      VoximplantLegacy.login(loginManagerGlobal.username, loginManagerGlobal.password);
-    } else if (loginManagerGlobal.myuser !== '' && loginManagerGlobal.password !== '') {
-      VoximplantLegacy.requestOneTimeKey(loginManagerGlobal.fullUserName);
-    } else {
-      loginManagerGlobal.emit('onConnected');
-    }
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.ConnectionClosed,
-  () => {
-    console.log('Connection closed');
-    loginManagerGlobal.connected = false;
-    loginManagerGlobal.loggedIn = false;
-    loginManagerGlobal.emit('onConnectionClosed');
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.LoginSuccessful,
-  (loginSuccessful) => {
-    console.log('Login successful ' + loginSuccessful.displayName);
-    loginManagerGlobal.displayName = loginSuccessful.displayName;
-    loginManagerGlobal.loggedIn = true;
-    loginManagerGlobal.processingPushNotification = false;
-
-    // save acceess and refresh token to default preferences to login using
-    // access token on push notification, if the connection to Voximplant Cloud
-    // is closed
-    const loginTokens = loginSuccessful.loginTokens;
-    if (loginTokens !== null) {
-      DefaultPreference.set('accessToken', loginTokens.accessToken);
-      DefaultPreference.set('refreshToken', loginTokens.refreshToken);
-      DefaultPreference.set('accessExpire', loginTokens.accessExpire.toString());
-      DefaultPreference.set('refreshExpire', loginTokens.refreshExpire.toString());
-    } else {
-      console.error("LoginSuccessful: login tokens are invalid");
-    }
-    loginManagerGlobal.registerPushToken();
-    loginManagerGlobal.emit('onLoggedIn', loginSuccessful.displayName);
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.LoginFailed,
-  (loginFailed) => {
-    console.log('Login failed: error code: ' + loginFailed.errorCode);
-    loginManagerGlobal.loggedIn = false;
-    loginManagerGlobal.emit('onLoginFailed', loginFailed.errorCode);
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.IncomingCall,
-  (incomingCall) => {
-    if (loginManagerGlobal.currentAppState !== "active") {
-      console.log('LoginManager: Incoming call: is video ' + incomingCall.videoCall);
-      PushManager.showLocalNotification(incomingCall.from);
-    }
-  }
-);
-
-DeviceEventEmitter.addListener(
-  VoximplantLegacy.Events.OneTimeKeyGenerated,
-  (oneTimeKeyGenerated) => {
-    console.log('LoginManager: OneTimeKeyGenerated: key: ' + oneTimeKeyGenerated.key);
-    let hash = md5.hex_md5(oneTimeKeyGenerated.key + "|"
-      + md5.hex_md5(loginManagerGlobal.myuser + ":voximplant.com:"
-        + loginManagerGlobal.password));
-    loginManagerGlobal.continueLoginWithOneTimeKey(loginManagerGlobal.fullUserName, hash);
-  }
+	VoximplantLegacy.Events.IncomingCall,
+	(incomingCall) => {
+		if (loginManagerGlobal.currentAppState !== "active") {
+			console.log('LoginManager: Incoming call: is video ' + incomingCall.videoCall);
+			PushManager.showLocalNotification(incomingCall.from);
+		}
+	}
 );
 
 const handlersGlobal = {};
 
 export default class LoginManager {
 
-  static myInstance = null;
-  connected = false;
-  loggedIn = false;
-  processingPushNotification = false;
-  displayName = '';
-  currentAppState = "inactive";
-  fullUserName = '';
-  myuser = '';
-  username = '';
-  password = '';
+	static myInstance = null;
+	client = null;
+	processingPushNotification = false;
+	displayName = '';
+	currentAppState = "inactive";
+	fullUserName = '';
+	myuser = '';
+	username = '';
+	password = '';
 
-  static getInstance() {
-    if (this.myInstance === null) {
-      this.myInstance = new LoginManager();
-    }
-    return this.myInstance;
-  }
+	static getInstance() {
+		if (this.myInstance === null) {
+			this.myInstance = new LoginManager();
+		}
+		return this.myInstance;
+	}
 
-  constructor() {
-    VoximplantLegacy.init();
-    // Connection to the Voximplant Clound is stayed alive on reloading of the app's 
-    // JavaScript code. Calling "closeConnection" API here makes the SDK and app states 
-    // synchronized.
-    VoximplantLegacy.closeConnection();
-    AppState.addEventListener("change", (...args) => this._handleAppStateChange(...args));
-  }
+	constructor() {
+		this.client = Voximplant.getClientInstance();
+		// Connection to the Voximplant Clound is stayed alive on reloading of the app's 
+		// JavaScript code. Calling "disconnect" API here makes the SDK and app states 
+		// synchronized.
+		PushManager.init();
+		this.client.disconnect();
+		AppState.addEventListener("change", (...args) => this._handleAppStateChange(...args));
+	}
 
-  _handleAppStateChange(newState) {
-    console.log("Current app state changed to " + newState);
-    this.currentAppState = newState;
-  }
+	loginWithPassword(user, password) {
+		this.username = user;
+		this.password = password;
+		(async() => {
+			try {
+				let state = await this.client.getClientState();
+				if (state === ClientState.DISCONNECTED) {
+					await this.client.connect();
+				} 
+				let authResult = await this.client.login(user, password);
+				this._processLoginSuccess(authResult);
+			} catch (e) {
+				console.log('LoginManager: loginWithPassword ' + e.name);
+				switch (e.name) {
+					case ClientEvents.ConnectionFailed:
+						this._emit('onConnectionFailed', e.message);
+						break;
+					case ClientEvents.AuthResult:
+						this._emit('onLoginFailed', e.code);
+						break;
+				}
+			}
+		})();
+	}
 
-  connect(connectivityCheck) {
-    VoximplantLegacy.connect({connectivityCheck})
-  }
+	loginWithOneTimeKey(user, password) {
+		this.fullUserName = user;
+		this.myuser = user.split('@')[0];
+		this.password = password;
+		(async() => {
+			try {
+				let state = await this.client.getClientState();
+				if (state === ClientState.DISCONNECTED) {
+					await this.client.connect();
+				} 
+				await this.client.requestOneTimeLoginKey(user);
+			} catch (e) {
+				console.log('LoginManager: loginWithPassword ' + e.name);
+				switch (e.name) {
+					case ClientEvents.ConnectionFailed:
+						this._emit('onConnectionFailed', e.message);
+						break;
+					case ClientEvents.AuthResult:
+						if (e.code === 302) {
+							let hash = md5.hex_md5(e.key + "|"
+								+ md5.hex_md5(this.myuser + ":voximplant.com:"
+									+ this.password));
+							try {
+								let authResult = await this.client.loginWithOneTimeKey(this.fullUserName, hash);
+								this._processLoginSuccess(authResult);
+							} catch (e1) {
+								this._emit('onLoginFailed', e1.code);
+							}
+							
+						}
+						break;
+				}
+			}
+			
+		})();
+	}
 
-  loginWithPassword(user, password) {
-    this.username = user;
-    this.password = password;
-    if (!this.connected) {
-      this.connect(false);
-    } else {
-    VoximplantLegacy.login(user, password);
-    }
-  }
+	loginWithToken() {
+		(async() => {
+			try {
+				let state = await this.client.getClientState();
+				if (state === ClientState.DISCONNECTED) {
+					await this.client.connect();
+				} 
+				if (state !== ClientState.LOGGED_IN) {
+					const username = await DefaultPreference.get('usernameValue');
+					const accessToken = await DefaultPreference.get('accessToken');
+					console.log('LoginManager: loginWithToken: user: ' + username + ', token: ' + accessToken );
+					const authResult = await this.client.loginWithToken(username + '.voximplant.com', accessToken);
+					this._processLoginSuccess(authResult);
+				}
+			} catch (e) {
+				console.log('LoginManager: loginWithToken: ' + e.name);
+				if (e.name === ClientEvents.AuthResult) {
+					console.log('LoginManager: loginWithToken: error code: ' + e.code);
+				}
+			}
+		})();
+	}
 
-  loginWithOneTimeKey(user, password) {
-    this.fullUserName = user;
-    this.myuser = user.split('@')[0];
-    this.password = password;
-    if (!this.connected) {
-      this.connect(false);
-    } else {
-    VoximplantLegacy.requestOneTimeKey(user);
-    }
-  }
+	logout() {
+		(async() => {
+			await this.client.disconnect();
+			this.unregisterPushToken();
+			this._emit('onConnectionClosed');
+		})();
+	}
 
-  continueLoginWithOneTimeKey(user, hash) {
-    VoximplantLegacy.loginUsingOneTimeKey(user, hash);
-  }
+	registerPushToken() {
+		this.client.registerPushNotificationsToken(PushManager.getPushToken());
+	}
 
-  logout() {
-    VoximplantLegacy.closeConnection();
-  }
+	unregisterPushToken() {
+		this.client.unregisterPushNotificationsToken(PushManager.getPushToken());
+	}
 
-  registerPushToken() {
-    VoximplantLegacy.registerForPushNotifications(PushManager.getPushToken());
-  }
+	pushNotificationReceived(notification) {
+		// While this flag is true, login is performed via access tokens.
+		// This glag will reset to false once logged in.
+		this.processingPushNotification = true;
+		(async() => {
+			await this.loginWithToken();
+			this.client.handlePushNotification(notification);
+		})();
+	}
 
-  unregisterPushToken() {
-    VoximplantLegacy.unregisterFromPushNotifications(PushManager.getPushToken());
-  }
+	on(event, handler) {
+		if (!handlersGlobal[event]) handlersGlobal[event] = [];
+		handlersGlobal[event].push(handler);
+	}
 
-  pushNotificationReceived(notification) {
-    // While this flag is true, login is performed via access tokens.
-    // This glag will reset to false once logged in.
-    this.processingPushNotification = true;
-    if (!this.connected) {
-      const isConnectivityCheck = false;
-      this.connect(isConnectivityCheck);
-    } else if (!this.loggedIn) {
-      DefaultPreference.get('usernameValue').then(
-        function (username) {
-          DefaultPreference.get('accessToken').then(
-            function (accessToken) {
-              VoximplantLegacy.loginUsingAccessToken(username + ".voximplant.com", accessToken);
-            });
-        });
-    }
-    VoximplantLegacy.handlePushNotification(notification);
-  }
+	off(event, handler) {
+		if (handlersGlobal[event]) {
+			handlersGlobal[event] = handlersGlobal[event].filter(v => v !== handler);
+		}
+	}
 
-  on(event, handler) {
-    if (!handlersGlobal[event]) handlersGlobal[event] = [];
-    handlersGlobal[event].push(handler);
-  }
+	_emit(event, ...args) {
+		const handlers = handlersGlobal[event];
+		if (handlers) {
+			for (const handler of handlers) {
+				handler(...args);
+			}
+		}
+	}
 
-  off(event, handler) {
-    if (handlersGlobal[event]) {
-      handlersGlobal[event] = handlersGlobal[event].filter(v => v !== handler);
-    }
-  }
+	_handleAppStateChange(newState) {
+		console.log("Current app state changed to " + newState);
+		this.currentAppState = newState;
+	}
 
-  emit(event, ...args) {
-    const handlers = handlersGlobal[event];
-    if (handlers) {
-      for (const handler of handlers) {
-        handler(...args);
-      }
-    }
-  }
+	_processLoginSuccess(authResult) {
+		this.displayName = authResult.displayName;
+		this.processingPushNotification = false;
+
+		// save acceess and refresh token to default preferences to login using
+		// access token on push notification, if the connection to Voximplant Cloud
+		// is closed
+		const loginTokens = authResult.tokens;
+		if (loginTokens !== null) {
+			DefaultPreference.set('accessToken', loginTokens.accessToken);
+			DefaultPreference.set('refreshToken', loginTokens.refreshToken);
+			DefaultPreference.set('accessExpire', loginTokens.accessExpire.toString());
+			DefaultPreference.set('refreshExpire', loginTokens.refreshExpire.toString());
+		} else {
+			console.error("LoginSuccessful: login tokens are invalid");
+		}
+		this.registerPushToken();
+		this._emit('onLoggedIn', authResult.displayName);
+	}
 }
 
 const loginManagerGlobal = LoginManager.getInstance();
